a84a10a4c5c8e08e37f26b506e40eb065c695e60
Roland van Laar <roland@micite.net>
2011-06-06 12:43:22 +0200

    Added different settings and urls for the different django versions.

--- src/djangorecipe/recipe.py
+++ src/djangorecipe/recipe.py
@@ -10,130 +10,9 @@ from zc.buildout import UserError
 import zc.recipe.egg
 import setuptools
 
-script_template = {
-    'wsgi': '''
-
-%(relative_paths_setup)s
-import sys
-sys.path[0:0] = [
-  %(path)s,
-  ]
-%(initialization)s
-import %(module_name)s
-
-application = %(module_name)s.%(attrs)s(%(arguments)s)
-''',
-    'fcgi': '''
-
-%(relative_paths_setup)s
-import sys
-sys.path[0:0] = [
-  %(path)s,
-  ]
-%(initialization)s
-import %(module_name)s
-
-%(module_name)s.%(attrs)s(%(arguments)s)
-'''
-}
+import boilerplate
 
 
-settings_template = '''
-import os
-
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '%(project)s.db'
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
-TIME_ZONE = 'America/Chicago'
-
-LANGUAGE_CODE = 'en-us'
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = %(media_root)s
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/admin_media/'
-
-# Don't share this with anybody.
-SECRET_KEY = '%(secret)s'
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.doc.XViewMiddleware',
-)
-
-ROOT_URLCONF = '%(urlconf)s'
-
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.admin',
-)
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-)
-
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-
-'''
-
-production_settings = '''
-from %(project)s.settings import *
-'''
-
-development_settings = '''
-from %(project)s.settings import *
-DEBUG=True
-TEMPLATE_DEBUG=DEBUG
-'''
-
-urls_template = '''
-from django.conf.urls.defaults import patterns, include, handler500
-from django.conf import settings
-from django.contrib import admin
-admin.autodiscover()
-
-handler500 # Pyflakes
-
-urlpatterns = patterns(
-    '',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
-)
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-         {'document_root': settings.MEDIA_ROOT}),
-    )
-'''
 
 class Recipe(object):
     def __init__(self, buildout, name, options):
@@ -319,6 +198,12 @@ class Recipe(object):
     def create_project(self, project_dir):
         os.makedirs(project_dir)
 
+        version = self.options['version']
+        if version == 'trunk':
+            configuration = boilerplate.django_trunk
+        else:
+
+
         template_vars = {'secret': self.generate_secret()}
         template_vars.update(self.options)
 
@@ -354,7 +239,7 @@ class Recipe(object):
         for protocol in ('wsgi', 'fcgi'):
             zc.buildout.easy_install.script_template = \
                 zc.buildout.easy_install.script_header + \
-                    script_template[protocol]
+                    boilerplate.script_template[protocol]
             if self.options.get(protocol, '').lower() == 'true':
                 project = self.options.get('projectegg',
                                            self.options['project'])
@@ -365,10 +250,10 @@ class Recipe(object):
                                      protocol),
                           'djangorecipe.%s' % protocol, 'main')],
                         ws,
-                        self.options['executable'],
+                        selfg.options['executable'],
                         self.options['bin-directory'],
                         extra_paths=extra_paths,
-                        arguments= "'%s.%s', logfile='%s'" % (
+                        arguments="'%s.%s', logfile='%s'" % (
                             project, self.options['settings'],
                             self.options.get('logfile'))))
         zc.buildout.easy_install.script_template = _script_template
