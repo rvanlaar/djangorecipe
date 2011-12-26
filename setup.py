92b9f16b9cfee59cb82bc01afb44147405a20260
Roland van Laar <roland@micite.net>
2011-12-26 20:55:12 +0100

    Added comma to list of classifiers.

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(name='djangorecipe',
         'Topic :: Software Development :: Build Tools',
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.5'
-        'Programming Language :: Python :: 2.6'
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         ],
