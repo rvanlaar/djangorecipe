4a1f300a22cf12efa7842f9aad2064815673a82d
Reinout van Rees <reinout@vanrees.org>
2012-05-04 15:15:35 +0200

    Renamed .txt files to .rst so that we get a great readme on github.

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ def read_file(name):
     return open(os.path.join(os.path.dirname(__file__),
                              name)).read()
 
-readme = read_file('README.txt')
-changes = read_file('CHANGES.txt')
+readme = read_file('README.rst')
+changes = read_file('CHANGES.rst')
 
 setup(name='djangorecipe',
       version=version,
