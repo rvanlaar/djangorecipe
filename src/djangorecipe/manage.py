2dcf3d2147c9a5ead54ad37b33fddb0818380732
Roland van Laar <roland@micite.net>
2012-05-20 12:01:14 +0200

    Refactored code.
    Try/Except block is now only for the code needing the exception.

--- src/djangorecipe/manage.py
+++ src/djangorecipe/manage.py
@@ -5,15 +5,16 @@ from django.core import management
 def main(settings_file):
     try:
         mod = __import__(settings_file)
-        components = settings_file.split('.')
-        for comp in components[1:]:
-            mod = getattr(mod, comp)
-
     except ImportError:
         # XXX: Hack for python < 2.6
         _, e, _ = sys.exc_info()
         sys.stderr.write("Error loading the settings module '%s': %s"
                             % (settings_file, e))
         sys.exit(1)
+
+    else:
+        components = settings_file.split('.')
+        for comp in components[1:]:
+            mod = getattr(mod, comp)
 
     management.execute_manager(mod)
