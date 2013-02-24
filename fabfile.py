00a01829e9bfab4a0b77771ecfe6aa550133d38d
Roland van Laar <roland@micite.net>
2013-02-24 11:47:21 +0100

    Make is used for building and testing on travis.
    This removes the need for the fabfile.

--- fabfile.py
+++
@@ -1,8 +0,0 @@
-from fabric.api import local
-
-def build():
-    """Create an in-place installation and run the tests."""
-
-    local('python bootstrap.py')
-    local('./bin/buildout -v')
-    local('./bin/test')
